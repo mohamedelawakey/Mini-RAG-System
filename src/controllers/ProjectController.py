03ccc9f866e3b3f93a07f247c550a1f66b09a242
MohamedAlawakey <mohamedelawakey@gmail.com>
2025-09-18 08:07:27 +0300

    add project controller to manage project directories

---
+++ src/controllers/ProjectController.py
@@ -0,0 +1,20 @@
+from .BaseController import BaseController
+import os
+
+
+class ProjectController(BaseController):
+    def __init__(self):
+        super().__init__()
+
+    def get_project_path(self, project_id: str):
+        """
+        Return the path for a project,
+        creating the directory if it doesn't exist
+        """
+
+        project_dir = os.path.join(self.file_dir, project_id)
+
+        if not os.path.exists(project_dir):
+            os.makedirs(project_dir)
+
+        return project_dir
