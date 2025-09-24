225de1ed6a71755f53436ada9c91053c373dfb07
MohamedAlawakey <mohamedelawakey@gmail.com>
2025-09-24 16:08:01 +0300

    add enum for supported file formats (TXT, PDF)

--- src/models/enums/ProcessingEnumerations.py
+++ src/models/enums/ProcessingEnumerations.py
@@ -2,5 +2,9 @@ from enum import Enum
 
 
 class ProcessingEnumerations(Enum):
+    """
+    Defines an enum for supported file formats (TXT, PDF).
+    Ensures consistency when handling file types.
+    """
     TXT = ".txt"
     PDF = ".pdf"
