48edb78633f8da2e8eb80ae8169cd10fc1bc3eb1
MohamedAlawakey <mohamedelawakey@gmail.com>
2025-09-24 15:56:31 +0300

    edit the implementation (2) data upload API with validation and async saving and processing files and chunking

--- src/Routes/data.py
+++ src/Routes/data.py
@@ -2,10 +2,11 @@
 from fastapi import APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Settings
-from controllers import DataController
+from controllers import DataController, ProcessController
 from models import ResponseSignal
 import aiofiles
 import logging
+from .schemes.data import ProcessRequest
 
 # Logger instance for error tracking
 logger = logging.getLogger("uvicorn.error")
@@ -77,3 +78,29 @@ async def upload_data(project_id: str,
                 "file_id": file_id
             }
     )
+
+
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id: str, process_request: ProcessRequest):
+    file_id = process_request.file_id
+    chunk_size = process_request.chunk_size
+    overlap_size = process_request.overlap_size
+    process_controller = ProcessController(project_id=project_id)
+    file_content = process_controller.get_file_content(file_id=file_id)
+
+    file_chunks = process_controller.process_file_content(
+        file_content=file_content,
+        file_id=file_id,
+        chunk_size=chunk_size,
+        overlap_size=overlap_size
+    )
+
+    if file_chunks is None or len(file_chunks) == 0:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.PROCESSING_FAILED.value
+            }
+        )
+
+    return file_chunks
